df4c269699710bdd22ac2263d3e08092f8fac2c6
amitchitkara0004 <amitchitkara0004@gmail.com>
2025-12-29 00:01:03 +0530

    assignment-1

---
+++ assigment1.py
@@ -0,0 +1,15 @@
+#solution-1
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+print("Addition:",num1+num2)
+print("Subtraction:",num1-num2)
+print("Multiplication:",num1*num2)
+print("Division:",num1/num2)
+
+
+
+
+#solution-2
+first_name=input("enter the first name:")
+last_name=input("enter the last name:")
+print("Hello,",first_name,last_name,"!","Welcome to the python program")
